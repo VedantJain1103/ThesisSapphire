68cc879e988a4eb1fa5d8148450d1fe9b6771a00
Vedant <vedantjain35@gmail.com>
2023-08-09 03:45:12 +0530

    FrontEnd modified

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('welcome', { title: 'Thesis Sapphire' });
 });
 
 router.get('/signIn', function (req, res, next) {
@@ -13,5 +13,27 @@ router.get('/signIn', function (req, res, next) {
 router.get('/register', function (req, res, next) {
   res.render('accounts/register');
 });
+
+router.get('/temp', function (req, res, next) {
+  res.render('temp1');
+});
+
+router.get('/t2', function (req, res, next) {
+  // res.render('temp1.haml');
+  res.render('pt')
+});
+
+router.get('/team', function (req, res, next) {
+  // res.render('temp1.haml');
+  res.render('team')
+});
+
+
+router.get('/error', function (req, res, next) {
+  // res.render('temp1.haml');
+  res.render('error')
+});
+
+
 
 module.exports = router;
